b198d009cb0b3d794e9dee6e9ae157f81fc46f23
datpthe163011 <99984414+datpthe163011@users.noreply.github.com>
2022-03-14 23:21:51 +0700

    Add files via upload

---
+++ ProduceController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Department;
+import model.Student;
+import model.Produce;
+
+/**
+ *
+ * @author pc
+ */
+public class ProduceController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ProdsDBContext dbProd = new ProdsDBContext();
+        ArrayList<Produce> prods = dbProd.getProds();
+        request.setAttribute("Prods", Prods);
+        
+        String raw_did = request.getParameter("Id");
+        if(raw_did == null || raw_did.trim().length()==0)
+            raw_did = "-1";
+        int did = Integer.parseInt(raw_did);
+        
+        StudentDBContext dbStudent = new StudentDBContext();
+        ArrayList<Student> students = dbStudent.getStudents(did);
+        request.setAttribute("students", students);
+        request.setAttribute("did", did);
+        
+        request.getRequestDispatcher("../view/student/search.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
